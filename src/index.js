507deeb0de20f198ace41c40e39bf4a45c010c11
rod773 <104315743+rod773@users.noreply.github.com>
2024-02-11 13:33:30 +0100

    initial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "bootstrap/dist/js/bootstrap.bundle";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
---
+++ src/redux/userSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initalState = {
+  name: "",
+  username: "",
+  email: "",
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initalState,
+  reducers: {
+    addUser: (state, action) => {
+      const { name, username, email } = action.payload;
+      state.name = name;
+      state.username = username;
+      state.email = email;
+    },
+    changeEmail: (state, action) => {
+      state.email = action.payload;
+    },
+  },
+});
+
+export const { addUser, changeEmail } = userSlice.actions;
+
+export default userSlice.reducer;
